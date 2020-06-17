2a3aafc77b292b92f3070c337c8e25828f78e323
Ronald Russo <RonaldRusso94@yahoo.com>
2020-06-17 17:24:35 -0400

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,9 @@
 import Login from '../src/components/Login';
+import Layout from '../src/components/Layout';
 const login = () => {
   return (
     <>
+      <Layout />
       <Login />
     </>
   );
