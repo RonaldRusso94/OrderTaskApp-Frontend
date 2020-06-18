85b8a3508ee34320584006080154846fa50abbc6
TNORTHERN <traynorthern@yahoo.com>
2020-06-18 13:35:03 -0500

    eslint added

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,16 +1,16 @@
-import { useState } from "react";
-import { useMutation } from "@apollo/react-hooks";
-import Link from "next/link";
-import Input from "./common/Input";
-import { REGISTER_MUTATION } from "../graphql/mutations/users";
-import { withApollo } from "../lib/apollo";
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import Link from 'next/link';
+import Input from './common/Input';
+import { REGISTER_MUTATION } from '../graphql/mutations/users';
+import { withApollo } from '../lib/apollo';
 
 const Register = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirm, setpasswordConfirm] = useState("");
-  const [formError, setFormError] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setpasswordConfirm] = useState('');
+  const [formError, setFormError] = useState('');
   const [
     register,
     { loading: mutationLoading, error: mutationError },
@@ -21,7 +21,7 @@ const Register = () => {
         {/* <!-- Login Section --> */}
         <div className="w-full md:w-1/2 flex flex-col">
           <div className="flex justify-center md:justify-start pt-12 md:pl-12 md:-mb-24">
-            <a href="#" className="bg-black text-white font-bold text-xl p-4">
+            <a href="/" className="bg-black text-white font-bold text-xl p-4">
               Logo
             </a>
           </div>
@@ -31,60 +31,59 @@ const Register = () => {
             <form
               className="flex flex-col pt-3 md:pt-8"
               onSubmit={(e) => {
-                setFormError("");
+                setFormError('');
                 e.preventDefault();
-                if (password !== passwordConfirm)
-                  setFormError("Passwords must match");
+                if (password !== passwordConfirm) setFormError('Passwords must match');
                 register({
                   variables: {
                     name: name || null,
                     email: email || null,
                     password: password || null,
-                    provider: "local",
+                    provider: 'local',
                     username: Date.now() + name,
                   },
                 })
                   .then((res) => {
-                    console.log("res", res);
+                    console.log('res', res);
                   })
                   .catch((err) => {
-                    console.log("err", err.graphQL);
+                    console.log('err', err.graphQL);
                   });
               }}
             >
               <Input
-                type={"text"}
-                id={"name"}
-                placeholder={"Full Name"}
-                htmlFor={"name"}
+                type="text"
+                id="name"
+                placeholder="Full Name"
+                htmlFor="name"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               />
 
               <Input
-                type={"email"}
-                id={"email"}
-                placeholder={"Email"}
-                htmlFor={"email"}
+                type="email"
+                id="email"
+                placeholder="Email"
+                htmlFor="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
 
               <Input
-                type={"password"}
-                id={"password"}
-                placeholder={"Password"}
-                htmlFor={"password"}
+                type="password"
+                id="password"
+                placeholder="Password"
+                htmlFor="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
 
               <Input
-                label={"Confirm Password"}
-                type={"password"}
-                id={"password2"}
-                placeholder={"Confirm Password"}
-                htmlFor={"password"}
+                label="Confirm Password"
+                type="password"
+                id="password2"
+                placeholder="Confirm Password"
+                htmlFor="password"
                 value={passwordConfirm}
                 onChange={(e) => setpasswordConfirm(e.target.value)}
               />
@@ -97,7 +96,8 @@ const Register = () => {
             </form>
             <div className="text-center pt-12 pb-12">
               <p>
-                Have an account?{" "}
+                Have an account?
+                {' '}
                 <Link href="/login">
                   <a href="/login" className="underline font-semibold">
                     Login here.
@@ -113,6 +113,7 @@ const Register = () => {
           <img
             className="object-cover w-full h-screen hidden md:block"
             src="https://source.unsplash.com/IXUM4cJynP0"
+            alt="unsplash"
           />
         </div>
       </div>
