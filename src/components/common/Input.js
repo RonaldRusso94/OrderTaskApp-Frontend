58a99e80fde1d6e0b0065a4db56bbc114b843e14
Ronald Russo <RonaldRusso94@yahoo.com>
2020-06-19 12:35:03 -0400

    Update Input.js

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,4 +1,5 @@
 import propTypes from 'prop-types';
+// TEST
 
 const Input = ({
   label,
@@ -10,7 +11,7 @@ const Input = ({
   name,
   labelClasses,
 }) => (
-  <div className="flex flex-col pt-4">
+  <div className='flex flex-col pt-4'>
     <label htmlFor={name || id} className={`${labelClasses || 'text-lg'}`}>
       {label || id.charAt(0).toUpperCase() + id.slice(1)}
       {/*  */}
@@ -22,7 +23,7 @@ const Input = ({
       placeholder={placeholder}
       value={value}
       onChange={onChange}
-      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"
+      className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline'
     />
   </div>
 );
